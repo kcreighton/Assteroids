4fba07a004bf311f8222bfba4b65f26e3e3d7024
Karen Creighton <kaycray@gmail.com>
2014-12-23 19:45:57 -0600

    Added images

--- main.py
+++ main.py
@@ -51,13 +51,40 @@ def main():
     fpsClock = pygame.time.Clock()
 
     # set up the window
-    DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+    DISPLAYSURF = pygame.display.set_mode((800, 600), 0, 32)
     pygame.display.set_caption('Assteroids')
+    ActiveObjects = None
+
+    # set up the colors
+    BLACK = (  0,   0,   0)
+    WHITE = (255, 255, 255)
+    RED = (255,   0,   0)
+    GREEN = (  0, 255,   0)
+    BLUE = (  0,   0, 255)
+    
+
+
 
     while True: # the main game loop 
-        
-        DISPLAYSURF.fill(BLACK)
+         
+        # draw on the surface object
+        DISPLAYSURF.fill(WHITE)
+        pygame.draw.polygon(DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+        pygame.draw.line(DISPLAYSURF, BLUE, (60, 60), (120, 60), 4)
+        pygame.draw.line(DISPLAYSURF, BLUE, (120, 60), (60, 120))
+        pygame.draw.line(DISPLAYSURF, BLUE, (60, 120), (120, 120), 4)
+        pygame.draw.circle(DISPLAYSURF, BLUE, (300, 50), 20, 0)
+        pygame.draw.ellipse(DISPLAYSURF, RED, (300, 250, 40, 80), 1)
+        pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
 
+        pixObj = pygame.PixelArray(DISPLAYSURF)
+        pixObj[480][380] = BLACK
+        pixObj[482][382] = BLACK
+        pixObj[484][384] = BLACK
+        pixObj[486][386] = BLACK
+        pixObj[488][388] = BLACK
+        del pixObj
+        
         movement(ActiveObjects)
 
         for event in pygame.event.get():
