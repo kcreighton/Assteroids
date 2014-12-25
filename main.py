a9f277db621f7080e3409757abe766dd9bf7ef08
t3chma <t3chma@users.noreply.github.com>
2014-12-24 22:58:26 -0600

    rearranged the code and updated movement.
    brakes still aren't working.

--- main.py
+++ main.py
@@ -9,42 +9,6 @@ CENTER = (400, 300)
 
 ObjectDrawQueue = []
 
-def inertia(Objects):
-    for Object in Objects:
-        Object.position[0] += Object.velocity[0]
-        Object.position[1] += Object.velocity[1]
-    # takes the velocity of all objects and moves them over time
-
-class Player:
-    def __init__(self, spawn = CENTER):
-        self.image = pygame.image.load('test.png') # the source image
-        self.sprite = pygame.image.load('test.png') # where the rotated image will be stored
-        self.health = 100
-        self.position = [spawn[0], spawn[1]]
-        self.velocity = [0, 0]
-        self.thrust = 1
-        ObjectDrawQueue.append(self)
-
-    def accelerate(self, direction):
-        self.velocity[0] += self.thrust * math.cos(direction)
-        self.velocity[1] += self.thrust * math.sin(direction)
-        
-    def brakes(self): # notworking properly
-        self.velocity[0] -= self.thrust * math.cos(self.direction)
-        if self.velocity[0] < 0:
-            self.velocity[0] = 0
-        self.velocity[1] -= self.thrust * math.sin(self.direction)
-        if self.velocity[1] < 0:
-            self.velocity[1] = 0
-            
-    def damage(self):
-        pass
-
-class Gun:
-    def __init__(self):
-        pass
-    
-
 def main():
     pygame.init()
 
@@ -63,28 +27,65 @@ def main():
         
         BACKGROUND.fill(BLACK)
         STAGE.fill(CLEAR)
+
         inertia(ObjectDrawQueue)
+        pressedList = pygame.key.get_pressed()
+
+        if pressedList[K_w] ==  True:
+            One.accelerate(3*math.pi/2)
+        elif pressedList[K_d] ==  True:
+            One.accelerate(0)
+        elif pressedList[K_s] ==  True:
+            One.accelerate(math.pi/2)
+        elif pressedList[K_a] ==  True:
+            One.accelerate(math.pi)
+        elif pressedList[K_SPACE] ==  True:
+            One.brakes()
 
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == KEYDOWN:
-                if event.key in (K_RIGHT, K_d):
-                    One.accelerate(0)
-                elif event.key in (K_DOWN, K_s):
-                    One.accelerate(math.pi/2)
-                elif event.key in (K_LEFT, K_a):
-                    One.accelerate(math.pi)
-                elif event.key in (K_UP, K_w):
-                    One.accelerate(3*math.pi/2)
             # more events go here
-        
+            
         for Object in ObjectDrawQueue:
             STAGE.blit(Object.sprite, Object.position)
         BACKGROUND.blit(STAGE, (0, 0))
         
         pygame.display.update()
         fpsClock.tick(FPS)
+
+def inertia(Objects): # takes the velocity of all objects and moves them over time
+    for Object in Objects:
+        Object.position[0] += Object.velocity[0]
+        Object.position[1] += Object.velocity[1]
+
+class Player:
+    def __init__(self, spawn = CENTER):
+        self.sprite = pygame.image.load('test.png')
+        self.health = 100
+        self.position = [spawn[0], spawn[1]]
+        self.velocity = [0, 0]
+        self.thrust = 1
+        ObjectDrawQueue.append(self)
+
+    def accelerate(self, direction):
+        self.velocity[0] += self.thrust * math.cos(direction)
+        self.velocity[1] += self.thrust * math.sin(direction)
+        
+    def brakes(self): # no tworking properly
+        self.velocity[0] -= self.thrust * math.cos(self.direction)
+        if self.velocity[0] < 0:
+            self.velocity[0] = 0
+        self.velocity[1] -= self.thrust * math.sin(self.direction)
+        if self.velocity[1] < 0:
+            self.velocity[1] = 0
+            
+    def damage(self):
+        pass
+
+class Gun:
+    def __init__(self):
+        pass
 
 main()
