7e609dc1d3852e1584343ed2f6424ef5696d51ec
t3chma <t3chma@users.noreply.github.com>
2014-12-28 20:17:53 -0600

    added basic aiming/firing

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import pygame, sys, math
 from pygame.locals import *
 
-# COLOR = (RRR, GGG, BBB)
+# COLOR = (RRR, GGG, BBB, AAA)
 BLACK   = (000, 000, 000)
+WHITE   = (255, 255, 255)
 CLEAR   = (000, 000, 000, 000)
 
-CENTER = (400, 300)
+WINDOWWIDTH = 1800
+WINDOWHEIGTH = 1000
+CENTER = (WINDOWWIDTH/2, WINDOWHEIGTH/2)
 
 ObjectDrawQueue = []
 
 def main():
     pygame.init()
 
-    FPS = 30 # frames per second setting
+    FPS = 60 # frames per second setting
     fpsClock = pygame.time.Clock()
 
     # set up the window
-    BACKGROUND = pygame.display.set_mode((800, 600), 0, 32)
+    BACKGROUND = pygame.display.set_mode((WINDOWWIDTH,  WINDOWHEIGTH), 0, 32)
     STAGE = BACKGROUND.convert_alpha()
     PHYSICS = BACKGROUND.convert_alpha()
     pygame.display.set_caption('Assteroids')
 
     One = Player()
+    mousePressed = False
 
     while True: # the main game loop 
         
@@ -29,23 +33,46 @@ def main():
         STAGE.fill(CLEAR)
 
         inertia(ObjectDrawQueue)
+        mousePosition = pygame.mouse.get_pos()
         pressedList = pygame.key.get_pressed()
-
+        
         if pressedList[K_w] ==  True:
-            One.accelerate(3*math.pi/2)
+            if pressedList[K_d] ==  True:
+                One.accelerate(7*math.pi/2)
+            else:
+                One.accelerate(3*math.pi/2)
         elif pressedList[K_d] ==  True:
-            One.accelerate(0)
+            if pressedList[K_s] ==  True:
+                One.accelerate(math.pi/4)
+            else:
+                One.accelerate(0)
         elif pressedList[K_s] ==  True:
-            One.accelerate(math.pi/2)
+            if pressedList[K_a] ==  True:
+                One.accelerate(3*math.pi/4)
+            else:
+                One.accelerate(math.pi/2)
         elif pressedList[K_a] ==  True:
-            One.accelerate(math.pi)
-        elif pressedList[K_SPACE] ==  True:
+            if pressedList[K_w] ==  True:
+                One.accelerate(5*math.pi/4)
+            else:
+                One.accelerate(math.pi)
+        if pressedList[K_SPACE] ==  True:
             One.brakes()
-
+        if pressedList[K_r] ==  True:
+            One.respawn()
+        if pressedList[K_t] ==  True:
+            One.respawn(preserveVelocity = True)
+        if mousePressed ==  True:
+            pygame.draw.line(STAGE, WHITE, One.center, mousePosition, 4)
+            
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                mousePressed = True
+            if event.type == MOUSEBUTTONUP:
+                mousePressed = False
             # more events go here
             
         for Object in ObjectDrawQueue:
@@ -59,12 +86,15 @@ def inertia(Objects): # takes the velocity of all objects and moves them over ti
     for Object in Objects:
         Object.position[0] += Object.velocity[0]
         Object.position[1] += Object.velocity[1]
+        Object.center[0] += Object.velocity[0]
+        Object.center[1] += Object.velocity[1]
 
 class Player:
     def __init__(self, spawn = CENTER):
         self.sprite = pygame.image.load('test.png')
         self.health = 100
         self.position = [spawn[0], spawn[1]]
+        self.center = [spawn[0] + 16, spawn[1] + 16]
         self.velocity = [0, 0]
         self.thrust = 1
         ObjectDrawQueue.append(self)
@@ -81,11 +111,16 @@ class Player:
         if self.velocity[1] < 0:
             self.velocity[1] = 0
             
+    def respawn(self, spawn = CENTER, preserveVelocity = False):
+        self.position = [spawn[0], spawn[1]]
+        self.center = [spawn[0] + 16, spawn[1] + 16]
+        if preserveVelocity == False:
+            self.velocity = [0, 0]
+            
     def damage(self):
         pass
 
 class Gun:
     def __init__(self):
         pass
-
 main()
