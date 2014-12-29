5d5717d331d515de32f7a3c878b667fb8cc4e161
t3chma <t3chma@users.noreply.github.com>
2014-12-28 23:02:58 -0600

    object-orientated structure for "Actor" class
    made "Gun" class for actor

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def main():
     PHYSICS = BACKGROUND.convert_alpha()
     pygame.display.set_caption('Assteroids')
 
-    One = Player()
-    mousePressed = False
+    One = Actor()
+    One.aim()
 
     while True: # the main game loop 
         
@@ -33,46 +33,50 @@ def main():
         STAGE.fill(CLEAR)
 
         inertia(ObjectDrawQueue)
-        mousePosition = pygame.mouse.get_pos()
+        One.newAim()
         pressedList = pygame.key.get_pressed()
+       
+       # <Movment>
         
         if pressedList[K_w] ==  True:
             if pressedList[K_d] ==  True:
-                One.accelerate(7*math.pi/2)
+                One.accelerate(7*math.pi/4)
+            elif pressedList[K_a] ==  True:
+                One.accelerate(5*math.pi/4)
             else:
                 One.accelerate(3*math.pi/2)
-        elif pressedList[K_d] ==  True:
-            if pressedList[K_s] ==  True:
-                One.accelerate(math.pi/4)
-            else:
-                One.accelerate(0)
         elif pressedList[K_s] ==  True:
-            if pressedList[K_a] ==  True:
+            if pressedList[K_d] ==  True:
+                One.accelerate(math.pi/4)
+            elif pressedList[K_a] ==  True:
                 One.accelerate(3*math.pi/4)
             else:
                 One.accelerate(math.pi/2)
+        elif pressedList[K_d] ==  True:
+            One.accelerate(0)
         elif pressedList[K_a] ==  True:
-            if pressedList[K_w] ==  True:
-                One.accelerate(5*math.pi/4)
-            else:
-                One.accelerate(math.pi)
+            One.accelerate(math.pi)
+            
+        # </Movement>
+            
         if pressedList[K_SPACE] ==  True:
             One.brakes()
         if pressedList[K_r] ==  True:
             One.respawn()
         if pressedList[K_t] ==  True:
             One.respawn(preserveVelocity = True)
-        if mousePressed ==  True:
-            pygame.draw.line(STAGE, WHITE, One.center, mousePosition, 4)
+        
+        if One.Gun.held ==  True:
+           One.Gun.hold(STAGE)
             
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == MOUSEBUTTONDOWN:
-                mousePressed = True
+                One.Gun.down()
             if event.type == MOUSEBUTTONUP:
-                mousePressed = False
+                One.Gun.up()
             # more events go here
             
         for Object in ObjectDrawQueue:
@@ -89,15 +93,26 @@ def inertia(Objects): # takes the velocity of all objects and moves them over ti
         Object.center[0] += Object.velocity[0]
         Object.center[1] += Object.velocity[1]
 
-class Player:
-    def __init__(self, spawn = CENTER):
+class Actor:
+    def __init__(self, spawn = CENTER, ai = "Player"):
         self.sprite = pygame.image.load('test.png')
         self.health = 100
         self.position = [spawn[0], spawn[1]]
         self.center = [spawn[0] + 16, spawn[1] + 16]
         self.velocity = [0, 0]
-        self.thrust = 1
+        self.thrust = 0.25
+        self.ai = ai
+        if ai == "Player":
+            self.aim = pygame.mouse.get_pos()
+        else:
+            self.aim = None
+        self.Gun = Gun(self)
         ObjectDrawQueue.append(self)
+
+    def newAim(control):
+        if self.ai == "Player":
+            self.aim = pygame.mouse.get_pos()
+            self.Gun.aim = pygame.mouse.get_pos()
 
     def accelerate(self, direction):
         self.velocity[0] += self.thrust * math.cos(direction)
@@ -121,6 +136,18 @@ class Player:
         pass
 
 class Gun:
-    def __init__(self):
-        pass
+    def __init__(self, Owner = None):
+        self.aim = Owner.aim
+        self.position = Owner.center
+        self.held = False
+        Owner.Gun = self
+    
+    def down(self):
+        self.held = True
+        
+    def up(self):
+        self.held = False
+    
+    def hold(self, SURFACE):
+        pygame.draw.line(SURFACE, WHITE, self.position, self.aim, 3)
 main()
